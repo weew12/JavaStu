85ae7c49ffbf182954b88eaab6361a93a93cab16
weew12 <2528566339@qq.com>
2019-10-31 17:17:07 +0800

    chang info

--- java project/javaStu/javaweb02/src/weew12/web/servlet/Aservlet.java
+++ java project/javaStu/javaweb02/src/weew12/web/servlet/Aservlet.java
@@ -1,14 +1,28 @@
 package weew12.web.servlet;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
 /**
  * @Classname Aservlet
  * @Description
+ *              配置servlet 两种方式：
+ *              方式1、在web.xml 中配置
+                *     <servlet>
+                *         <servlet-name>Aservlet</servlet-name>
+                *         <servlet-class>weew12.web.servlet.Aservlet</servlet-class>
+                *     </servlet>
+                *     <servlet-mapping>
+                *         <servlet-name>Aservlet</servlet-name>
+                *         <url-pattern>/ServletA</url-pattern>
+                *     </servlet-mapping>
+ *              方式2、直接使用注解
+*                @WebServlet(name = "Aservlet", urlPatterns = {"/ServletA"})
  * @Date 2019-10-30
  * @Created by 枫weew12
  */
+@WebServlet(name = "Aservlet", urlPatterns = {"/ServletA"})
 public class Aservlet implements Servlet {
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
@@ -23,7 +37,7 @@ public class Aservlet implements Servlet {
 
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        System.out.println("service()");
+        System.out.println("serviceA()");
     }
 
     @Override
